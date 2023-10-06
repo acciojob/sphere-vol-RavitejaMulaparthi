b143015da46a021262fde7280ec2b744e449a665
RavitejaMulaparthi <137281804+RavitejaMulaparthi@users.noreply.github.com>
2023-10-06 15:23:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function volume_sphere() {
     // Get the radius value from the "Radius" input field
-    const radius = parseFloat(document.getElementById('Radius').value);
+    const radius = parseFloat(document.getElementById('radius').value);
 
     // Check if the radius is a valid number
     if (!isNaN(radius) && radius >= 0) {
@@ -8,10 +8,10 @@ function volume_sphere() {
         const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 
         // Update the "Volume" field with the calculated volume
-        document.getElementById('Volume').value = volume.toFixed(2); // Display with two decimal places
+        document.getElementById('volume').value = volume.toFixed(2); // Display with two decimal places
     } else {
         // If the radius is not a valid number or is negative, display an error message
-        document.getElementById('Volume').value = 'Invalid Input';
+        document.getElementById('volume').value = 'Invalid Input';
     }
 }
 
